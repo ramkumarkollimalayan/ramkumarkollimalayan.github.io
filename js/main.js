d71c42603c94e4f9dd410f0a160616ac820bf5b5
Ramkumar Kollimalayan <kraamkumar@outlook.com>
2022-05-21 11:46:17 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+$(function() {
+    $('a[href*="#"]').bind('click', function(e) {
+        e.preventDefault();
+
+        var target = $(this).attr("href"); 
+        $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+        }, 600, function() {
+                location.hash = target;
+        });
+
+        return false;
+    });
+
+    $(window).scroll(function() {
+        var scrollDistance = $(window).scrollTop();
+
+        $('section').each(function(i) {
+            if ($(this).position().top <= scrollDistance) {
+                    $('.nav-item.active').removeClass('active');
+                    $('.nav-item').eq(i).addClass('active');
+            }
+        });
+    }).scroll();
+
+});
