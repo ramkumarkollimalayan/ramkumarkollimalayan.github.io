882132a14e121b785f55bef964e1038a228b8be3
Ramkumar Kollimalayan <kraamkumar@outlook.com>
2022-05-26 05:30:58 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -29,4 +29,56 @@ $(function() {
             }
         });
     }).scroll();
+
+    async function init () {
+        const node = document.querySelector("#type-text")
+        
+        await sleep(1000)
+        node.innerText = ""
+        await node.type('')
+        
+        while (true) {
+          await node.type('Frontend Developer')
+          await sleep(2000)
+          await node.delete('Frontend Developer')
+          await node.type('Senior UX/UI Developer')
+          await sleep(2000)
+          await node.delete('Senior UX/UI Developer')
+          await node.type('Web Designer')
+          await sleep(2000)
+          await node.delete('Web Designer')
+          await node.type('Master of Computer Applications')
+          await sleep(2000)
+          await node.delete('Master of Computer Applications')
+        }
+      }
+      
+      
+      // Source code 🚩
+      
+      const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+      
+      class TypeAsync extends HTMLSpanElement {
+        get typeInterval () {
+          const randomMs = 100 * Math.random()
+          return randomMs < 50 ? 10 : randomMs
+        }
+        
+        async type (text) {
+          for (let character of text) {
+            this.innerText += character
+            await sleep(this.typeInterval)
+          }
+        }
+        
+        async delete (text) {
+          for (let character of text) {
+            this.innerText = this.innerText.slice(0, this.innerText.length -1)
+            await sleep(this.typeInterval)
+          }
+        }
+      }
+      
+      customElements.define('type-async', TypeAsync, { extends: 'span' })
+      init()      
 });
